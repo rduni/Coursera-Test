6be024d6a358b5784075555f660e65ba2010b2f6
Rohit Datta <rohitdatta1998@gmail.com>
2020-08-27 09:54:29 +0530

    Jarvis Assistant

--- Jarvis Assistant/jarvis.py
+++ Jarvis Assistant/jarvis.py
@@ -64,7 +64,7 @@ def sendEmail(to, content):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
-    server.login('rohitdatta1998@gmail.com', 'DattaKutta')
+    server.login('yourmail', 'your password')
     server.sendmail('youremail@gmail.com', to, content)
     server.close()
 
